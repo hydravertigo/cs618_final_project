25b776581487aee1fead91c6a6568fe508886a7d
Lee Reynolds <leebreynolds@protonmail.com>
2025-10-26 18:24:17 -0400

    chore: add modules from lectures
    Signed-off-by: Lee Reynolds <leebreynolds@protonmail.com>

--- commands.sh
+++ commands.sh
@@ -4,4 +4,50 @@
 
 #npm install react-router-dom@6.21.0
 
-npm install jwt-decode@4.0.0
+#npm install jwt-decode@4.0.0
+
+npm install @apollo/client@3.9.5
+
+npm install compression@1.7.4
+
+npm install cross-env@7.0.3
+
+npm install dotenv@16.3.1
+
+npm install express@4.18.2
+
+npm install graphql@16.8.1
+
+npm install react-helmet-async@1.3.0
+
+npm install serve@14.2.5
+
+npm install slug@8.2.3
+
+npm install victory@36.9.1
+
+
+#
+#hello-cs618@0.0.0 /home/leereyno/Dropbox/classes/bsu/cs618/cs618_final_project
+#├── @commitlint/cli@18.4.3
+#├── @commitlint/config-conventional@18.4.3
+#├── @tanstack/react-query@5.12.2
+#├── @types/react-dom@18.3.7
+#├── @types/react@18.3.23
+#├── @vitejs/plugin-react@4.7.0
+#├── eslint-config-prettier@9.0.0
+#├── eslint-plugin-jsx-a11y@6.8.0
+#├── eslint-plugin-react-hooks@4.6.2
+#├── eslint-plugin-react-refresh@0.4.20
+#├── eslint-plugin-react@7.33.2
+#├── eslint@8.54.0
+#├── husky@8.0.3
+#├── jwt-decode@4.0.0
+#├── lint-staged@15.1.0
+#├── mongodb@6.3.0
+#├── prettier@3.1.0
+#├── react-dom@18.3.1
+#├── react-router-dom@6.21.0
+#├── react@18.3.1
+#└── vite@5.4.19
+#
