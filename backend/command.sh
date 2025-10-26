081e0302f34868d1953bdddce1e8365ed59d8c01
Lee Reynolds <leebreynolds@protonmail.com>
2025-10-26 18:32:10 -0400

    chore: add modules to backend from example code
    Signed-off-by: Lee Reynolds <leebreynolds@protonmail.com>

--- backend/command.sh
+++ backend/command.sh
@@ -6,4 +6,14 @@
 
 #npm install jsonwebtoken@9.0.2
 
-npm install express-jwt@8.4.1
+#npm install express-jwt@8.4.1
+
+## Match example code
+
+#npm install @apollo/server@4.10.0
+
+#npm install graphql@16.8.1
+
+#npm install serve@14.2.5
+
+#npm install uuid@9.0.1
